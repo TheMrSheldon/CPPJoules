5e2e8067d4f697ed051f2570d81cbf67e0390c0d
Tim <timhagenmail@googlemail.com>
2025-02-26 16:56:50 +0100

    added dummy PCM tracking

--- src/cppjoules/detail/pcmdevice.cpp
+++ src/cppjoules/detail/pcmdevice.cpp
@@ -9,9 +9,10 @@ using cppjoules::detail::PCMDevice;
 PCMDevice::PCMDevice(pcm::PCM &pcm) : pcm(pcm) {
     std::cout << pcm.getCPUBrandString() << std::endl;
     auto state1 = pcm.getSystemCounterState();
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     auto state2 = pcm.getSystemCounterState();
-    pcm::getConsumedEnergy(state1, state2);
-    pcm::getDRAMConsumedJoules(state1, state2);
+    std::cout << "Energy:        " << pcm::getConsumedEnergy(state1, state2) << std::endl;
+    std::cout << "Energy (DRAM): " << pcm::getDRAMConsumedJoules(state1, state2) << std::endl;
 }
 
 std::unique_ptr<PCMDevice> PCMDevice::getPCMDevice()
