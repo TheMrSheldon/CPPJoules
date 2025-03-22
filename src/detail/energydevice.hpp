0d13f3d91e3ef03ba05dc656f513f0200e121310
Tim <timhagenmail@googlemail.com>
2025-03-22 19:53:11 +0100

    added EnergyDevice virtual destructor

--- src/detail/energydevice.hpp
+++ src/detail/energydevice.hpp
@@ -19,6 +19,8 @@ namespace cppjoules::detail
   class EnergyDevice
   {
   public:
+    virtual ~EnergyDevice() = default;
+
     virtual Capability getCapabilities() const = 0;
     virtual std::map<std::string, unsigned long long> getEnergy() = 0;
   };
